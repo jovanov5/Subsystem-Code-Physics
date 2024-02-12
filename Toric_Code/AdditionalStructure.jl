6514c32eb3656363a6ca5ea07c85365217b064f9
Jovan Jovanovic <jjovanovic996@gmail.com>
2024-02-12 10:39:21 +0000

    tidy up

--- Toric_Code/AdditionalStructure.jl
+++ Toric_Code/AdditionalStructure.jl
@@ -552,3 +552,27 @@ function classical_gauge_stab(i::Integer, system)
     return PauliOperator(0x0, zeros(Bool, nbits), bits_z)
 end
 
+#
+#
+##################################################################################################################################
+
+# Iterators and other
+
+function iterate_measurements_only!(state::MixedDestabilizer, system, get_random_pauli::Function, full_steps::Integer)
+    """ Evolves the state by a set number of full steps (intensive variable).
+    state - the refernece to a state involved, it is mutable and will change
+    system - details of the geometry
+    get_random_pauli - defines the model
+    full_steps - the number of steps in units of system size
+    """
+
+    nbits = system.nbits
+
+    for i_n = 1:nbits
+        for i_s = 1:full_steps
+            projectrand!(state, get_random_pauli())
+        end
+    end
+    
+    return 0
+end
