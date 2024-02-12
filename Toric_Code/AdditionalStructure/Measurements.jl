5b536bdbca400ce032bacebcfc91d88619fb89e5
Jovan Jovanovic <jjovanovic996@gmail.com>
2024-02-12 16:51:41 +0000

    deepcopy hotfix

--- Toric_Code/AdditionalStructure/Measurements.jl
+++ Toric_Code/AdditionalStructure/Measurements.jl
@@ -1,77 +1,77 @@
 """ Measurement
-    - methods rely on the correct definiton of each `system` object.
+    - methods rely on the correct definiton of each `system` object
 """
 
 function entanglement_negativity(state::MixedDestabilizer,
-    system)
-"""this calculates the TEN of the state on a torus, using the usual quadpartition. 
-state            : state to be evolved. It is taken by reference (hence the ! in the name by convention).
-system           : system object that has knows everyhting!
-"""
+                                system)
+    """this calculates the TEN of the state on a torus, using the usual quadpartition. 
+    state            : state to be evolved. It is taken by reference (hence the ! in the name by convention).
+    system           : system object that has knows everyhting!
+    """
 
-S = system.S
-P_A = system.P_A
-P_B = system.P_B
-P_C = system.P_C
+    S = system.S
+    P_A = system.P_A
+    P_B = system.P_B
+    P_C = system.P_C
 
-T = stab_to_gf2(stabilizerview(state))'
+    T = stab_to_gf2(stabilizerview(state))'
 
-e_A = rank(T'*(P_A)*S*(P_A)*T)/2
-e_B = rank(T'*(P_B)*S*(P_B)*T)/2
-e_C = rank(T'*(P_C)*S*(P_C)*T)/2
-e_AB = rank(T'*(P_A+P_B)*S*(P_A+P_B)*T)/2
-e_AC = rank(T'*(P_A+P_C)*S*(P_A+P_C)*T)/2
-e_BC = rank(T'*(P_B+P_C)*S*(P_B+P_C)*T)/2
-e_ABC = rank(T'*(P_A+P_B+P_C)*S*(P_A+P_B+P_C)*T)/2
+    e_A = rank(T'*(P_A)*S*(P_A)*T)/2
+    e_B = rank(T'*(P_B)*S*(P_B)*T)/2
+    e_C = rank(T'*(P_C)*S*(P_C)*T)/2
+    e_AB = rank(T'*(P_A+P_B)*S*(P_A+P_B)*T)/2
+    e_AC = rank(T'*(P_A+P_C)*S*(P_A+P_C)*T)/2
+    e_BC = rank(T'*(P_B+P_C)*S*(P_B+P_C)*T)/2
+    e_ABC = rank(T'*(P_A+P_B+P_C)*S*(P_A+P_B+P_C)*T)/2
 
-e_topo = -e_A-e_B-e_C+e_AB+e_AC+e_BC-e_ABC
-return -e_topo
+    e_topo = -e_A-e_B-e_C+e_AB+e_AC+e_BC-e_ABC
+    return -e_topo
 end
 
 function entanglement_entropy_topo(state::MixedDestabilizer,
-system)
-"""this calculates the TEE of the state on a torus, using the usual quadpartition. 
-state            : state to be evolved. It is taken by reference (hence the ! in the name by convention).
-n                : total number of qubits.
-"""
+                            system)
+    """this calculates the TEE of the state on a torus, using the usual quadpartition. 
+    state            : state to be evolved. It is taken by reference (hence the ! in the name by convention).
+    n                : total number of qubits.
+    """
 
-r_A = system.r_A
-r_B = system.r_B
-r_C = system.r_C
-r_AB = system.r_AB
-r_BC = system.r_BC
-r_ABC = system.r_ABC
+    r_A = system.r_A
+    r_B = system.r_B
+    r_C = system.r_C
+    r_AB = system.r_AB
+    r_BC = system.r_BC
+    r_ABC = system.r_ABC
 
-e_A = entanglement_entropy(state,     # state to compute entropy for
-r_A,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_B = entanglement_entropy(state,     # state to compute entropy for
-r_B,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_C = entanglement_entropy(state,     # state to compute entropy for
-r_C,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_AB = entanglement_entropy(state,     # state to compute entropy for
-r_AB,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_AC = entanglement_entropy(state,     # state to compute entropy for
-[r_A; r_C],       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_BC = entanglement_entropy(state,     # state to compute entropy for
-r_BC,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
-e_ABC = entanglement_entropy(state,     # state to compute entropy for
-r_ABC,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
-Val(:rref) # algorithm to use (see documentation)
-)
+    e_A = entanglement_entropy(state,     # state to compute entropy for
+                r_A,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_B = entanglement_entropy(state,     # state to compute entropy for
+                r_B,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_C = entanglement_entropy(state,     # state to compute entropy for
+                r_C,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_AB = entanglement_entropy(state,     # state to compute entropy for
+                r_AB,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_BC = entanglement_entropy(state,     # state to compute entropy for
+                r_BC,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_ABC = entanglement_entropy(state,     # state to compute entropy for
+                r_ABC,       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:clip) # algorithm to use (see documentation)
+                )
+    e_AC = entanglement_entropy(state,     # state to compute entropy for
+                [r_A; r_C],       # subsystem as list of indices. Specifying full subsystem gives the van-neumann entropy
+                Val(:rref) # algorithm to use (see documentation)
+                )
 
-e_topo = -e_A-e_B-e_C+e_AB+e_AC+e_BC-e_ABC
-# print(e_topo)
-return -e_topo
+    e_topo = -e_A-e_B-e_C+e_AB+e_AC+e_BC-e_ABC
+    # print(e_topo)
+    return -e_topo
 end
--- Toric_Code/AdditionalStructure/NewTypes.jl
+++ Toric_Code/AdditionalStructure/NewTypes.jl
@@ -7,7 +7,7 @@ struct VertexSquareLattice
     cell_num::Int
     # Projectors and Ranges used in TEN and TEE measurments, assuming ususal 
     subsys::Int
-    cell_size::Int
+    subsys_size::Int
     P_A
     P_B
     P_C
@@ -27,7 +27,7 @@ struct EdgeSquareLattice
     cell_num::Int
     # Projectors and Ranges used in TEN and TEE measurments, assuming ususal 
     subsys::Int
-    cell_size::Int
+    subsys_size::Int
     P_A
     P_B
     P_C
@@ -54,21 +54,21 @@ function Init_VertexSquareLattice(subsys::Int)
     nbits = k*cell_num
     
     # For TEN: define the projection onto the ususal multipartitions of the torus
-    cell_size = k*L*subsys
-    P_A = Diagonal(repeat(vcat(repeat([true], cell_size), repeat([false], 3*cell_size)), 2))
-    P_B = Diagonal(repeat(vcat(repeat([false], cell_size), repeat([true], cell_size), repeat([false], 2*cell_size)), 2))
-    P_C = Diagonal(repeat(vcat(repeat([false], 2*cell_size), repeat([true], cell_size), repeat([false], cell_size)), 2))
+    subsys_size = k*L*subsys
+    P_A = Diagonal(repeat(vcat(repeat([true], subsys_size), repeat([false], 3*subsys_size)), 2))
+    P_B = Diagonal(repeat(vcat(repeat([false], subsys_size), repeat([true], subsys_size), repeat([false], 2*subsys_size)), 2))
+    P_C = Diagonal(repeat(vcat(repeat([false], 2*subsys_size), repeat([true], subsys_size), repeat([false], subsys_size)), 2))
     S = kron([false true; true false], Diagonal(repeat([true], k*L*L)));
 
     # For TEE: refined qubit indices for diferent continous partitions
-    r_A = 0*cell_size+1:1*cell_size
-    r_B = 1*cell_size+1:2*cell_size
-    r_C = 2*cell_size+1:3*cell_size
-    r_AB = 0*cell_size+1:2*cell_size
-    r_BC = 1*cell_size+1:3*cell_size
-    r_ABC = 0*cell_size+1:3*cell_size
+    r_A = 0*subsys_size+1:1*subsys_size
+    r_B = 1*subsys_size+1:2*subsys_size
+    r_C = 2*subsys_size+1:3*subsys_size
+    r_AB = 0*subsys_size+1:2*subsys_size
+    r_BC = 1*subsys_size+1:3*subsys_size
+    r_ABC = 0*subsys_size+1:3*subsys_size
 
-    return VertexSquareLattice(L, nbits, cell_num, subsys, cell_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
+    return VertexSquareLattice(L, nbits, cell_num, subsys, subsys_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
 end
 
 function Init_EdgeSquareLattice(subsys::Int)
@@ -80,19 +80,19 @@ function Init_EdgeSquareLattice(subsys::Int)
     nbits = k*cell_num
     
     # For TEN: define the projection onto the ususal multipartitions of the torus
-    cell_size = k*L*subsys # Misleading NAME, be careful!
-    P_A = Diagonal(repeat(vcat(repeat([true], cell_size), repeat([false], 3*cell_size)), 2))
-    P_B = Diagonal(repeat(vcat(repeat([false], cell_size), repeat([true], cell_size), repeat([false], 2*cell_size)), 2))
-    P_C = Diagonal(repeat(vcat(repeat([false], 2*cell_size), repeat([true], cell_size), repeat([false], cell_size)), 2))
+    subsys_size = k*L*subsys # Misleading NAME, be careful!
+    P_A = Diagonal(repeat(vcat(repeat([true], subsys_size), repeat([false], 3*subsys_size)), 2))
+    P_B = Diagonal(repeat(vcat(repeat([false], subsys_size), repeat([true], subsys_size), repeat([false], 2*subsys_size)), 2))
+    P_C = Diagonal(repeat(vcat(repeat([false], 2*subsys_size), repeat([true], subsys_size), repeat([false], subsys_size)), 2))
     S = kron([false true; true false], Diagonal(repeat([true], k*L*L)));
 
     # For TEE: refined qubit indices for diferent continous partitions
-    r_A = 0*cell_size+1:1*cell_size
-    r_B = 1*cell_size+1:2*cell_size
-    r_C = 2*cell_size+1:3*cell_size
-    r_AB = 0*cell_size+1:2*cell_size
-    r_BC = 1*cell_size+1:3*cell_size
-    r_ABC = 0*cell_size+1:3*cell_size
+    r_A = 0*subsys_size+1:1*subsys_size
+    r_B = 1*subsys_size+1:2*subsys_size
+    r_C = 2*subsys_size+1:3*subsys_size
+    r_AB = 0*subsys_size+1:2*subsys_size
+    r_BC = 1*subsys_size+1:3*subsys_size
+    r_ABC = 0*subsys_size+1:3*subsys_size
 
-    return EdgeSquareLattice(L, nbits, cell_num, subsys, cell_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
+    return EdgeSquareLattice(L, nbits, cell_num, subsys, subsys_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
 end
