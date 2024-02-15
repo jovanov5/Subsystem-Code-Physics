9ecb10edaa385a3567aafba9f50642cf465abfcd
Jovan Jovanovic <jjovanovic996@gmail.com>
2024-02-15 17:38:42 +0000

    kitaev honey (beta, needs fixing)

--- Toric_Code/AdditionalStructure/MICModels.jl
+++ Toric_Code/AdditionalStructure/MICModels.jl
@@ -9,7 +9,7 @@
     PauliZ = 3
 end
 
-@enum StabType begin
+@enum StabTypeTC begin
     Star = 1
     Plaquette = 2
     e_hor = 3
@@ -20,6 +20,12 @@ end
     f_ver = 8
 end
 
+@enum StabTypeHC begin
+    XX = 1
+    YY = 2
+    ZZ = 3
+end
+
 #
 #
 ####################################################################################################################################################
@@ -27,7 +33,7 @@ end
 # Some Models: Each functions most likely work on each a specific type of system.
 
 function toric_code(system::EdgeSquareLattice, stab_type_dist::DiscreteNonParametric)
-    """this defines the kitaev model. For any other (qubit) model changing this function should in principle suffice."""
+    """this defines the TC model. For any other (qubit) model changing this function should in principle suffice."""
 
     ncells = system.cell_num
 
@@ -37,13 +43,44 @@ function toric_code(system::EdgeSquareLattice, stab_type_dist::DiscreteNonParame
 
 
     # get a random pauli type (not the conversion since rand(pauli_type_dist) generates a number between 1 and 3)
-    stab_type::StabType = StabType(rand(stab_type_dist));
+    stab_type::StabTypeTC = StabTypeTC(rand(stab_type_dist));
     
     # now return the respective pauli operator
     if stab_type == Star || stab_type == Plaquette 
         return tc_stab(stab_type, random_cell, system)
     else
         return ribs_stab(stab_type, random_cell, system)
+    end
+end
+
+function kitaev_code(system::VertexHoneyLattice, stab_type_dist::DiscreteNonParametric)
+    """this defines the kitaev model. For any other (qubit) model changing this function should in principle suffice."""
+
+    ncells = system.cell_num
+    nbits = system.nbits
+    L = system.L
+
+    # get a random cell to measure
+    # note that cell going from 0 to ncells-1 is against typical julia convention, which indexes starting from 1
+    random_cell = rand(0:(ncells-1)) 
+
+
+    # get a random pauli type (not the conversion since rand(pauli_type_dist) generates a number between 1 and 3)
+    stab_type::StabTypeHC = StabTypeHC(rand(stab_type_dist));
+    
+    # now return the respective pauli operator    
+    if stab_type == XX::StabTypeHC
+        adjacent_cell = mod(random_cell+1, L) + div(random_cell, L) * L
+        bits = bit_string_ij(2*random_cell+1, 2*adjacent_cell+2, nbits)
+        return PauliOperator(0x0, bits, zeros(Bool, nbits))
+    elseif stab_type == YY::StabTypeHC
+        adjacent_cell = mod(random_cell, L) + mod(div(random_cell, L)+1, L) * L
+        bits = bit_string_ij(2*random_cell+2, 2*adjacent_cell+1, nbits)
+        return PauliOperator(0x0, bits, bits)
+    else
+        adjacent_cell = random_cell
+        bits = bit_string_ij(2*random_cell+1, 2*adjacent_cell+2, nbits)
+        return PauliOperator(0x0, zeros(Bool, nbits), bits)
     end
 end
 
@@ -69,13 +106,13 @@ end
 
 # Helper functions for the Model definitions!
 
-function tc_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
+function tc_stab(t::StabTypeTC, i::Integer, system::EdgeSquareLattice)
     """returns a four-body-pauli stabilier of the TC, acting on a site i
         Note the indexing descepancy, i is a site index (counting from 0)!"""
     
     n = system.nbits
     L = system.L
-    if t == Star::StabType
+    if t == Star::StabTypeTC
         if i == 0 # The Periodic Boundary Condition
             bits = bit_string_ijkl(1, 2, 2*L*L, 2*L*L-1, 2*L*L)
         elseif i < L
@@ -86,7 +123,7 @@ function tc_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
             bits = bit_string_ijkl(2*i+1, 2*i+2, 2*(i-L)+1, 2*(i-1)+2, 2*L*L)
         end
         return PauliOperator(0x0, bits, zeros(Bool, n));
-    elseif t == Plaquette::StabType
+    elseif t == Plaquette::StabTypeTC
         if i == L*L-1
             bits = bit_string_ijkl(2*L*L-1, 2*(L-1)+2, 2*L*(L-1)+1, 2*L*L, 2*L*L)
         elseif i >= L*(L-1)
@@ -100,13 +137,13 @@ function tc_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
     end
 end
 
-function ribs_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
+function ribs_stab(t::StabTypeTC, i::Integer, system::EdgeSquareLattice)
     """returns a one-to-two-body-pauli short ribbon operator, acting on a site i
         Note the indexing descepancy, i is a site index (counting from 0)!"""
     
     n = system.nbits
     L = system.L
-    if t == e_hor::StabType
+    if t == e_hor::StabTypeTC
         if i < L
             bits = bit_string_i(2*(i+L*(L-1))+1, 2*L*L)
         else
@@ -114,7 +151,7 @@ function ribs_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
         end
         return PauliOperator(0x0, zeros(Bool, n), bits);
 
-    elseif t == e_ver::StabType
+    elseif t == e_ver::StabTypeTC
         if mod(i, L) == 0
             bits = bit_string_i(2*(i+L-1)+2, 2*L*L)
         else
@@ -122,15 +159,15 @@ function ribs_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
         end
         return PauliOperator(0x0, zeros(Bool, n), bits);
 
-    elseif t == m_hor::StabType
+    elseif t == m_hor::StabTypeTC
         bits = bit_string_i(2*i+2, 2*L*L)
         return PauliOperator(0x0, bits, zeros(Bool, n));
 
-    elseif t == m_ver::StabType
+    elseif t == m_ver::StabTypeTC
         bits = bit_string_i(2*i+1, 2*L*L)
         return PauliOperator(0x0, bits, zeros(Bool, n));
 
-    elseif t == f_hor::StabType
+    elseif t == f_hor::StabTypeTC
         if i < L
             bits_z = bit_string_i(2*(i+L*(L-1))+1, 2*L*L)
         else
@@ -139,7 +176,7 @@ function ribs_stab(t::StabType, i::Integer, system::EdgeSquareLattice)
         bits_x = bit_string_i(2*i+2, 2*L*L)
         return PauliOperator(0x0, bits_x, bits_z);
 
-    elseif t == f_ver::StabType
+    elseif t == f_ver::StabTypeTC
         if mod(i, L) == 0
             bits_z = bit_string_i(2*(i+L-1)+2, 2*L*L)
         else
--- Toric_Code/AdditionalStructure/NewTypes.jl
+++ Toric_Code/AdditionalStructure/NewTypes.jl
@@ -40,6 +40,26 @@ struct EdgeSquareLattice
     r_ABC
 end
 
+struct VertexHoneyLattice # Isometric to the EdgeSquareLattice up to some sheering!
+    # Dimensions of lattice on the torus
+    L::Int
+    nbits::Int
+    cell_num::Int
+    # Projectors and Ranges used in TEN and TEE measurments, assuming ususal 
+    subsys::Int
+    subsys_size::Int
+    P_A
+    P_B
+    P_C
+    S
+    r_A
+    r_B
+    r_C
+    r_AB
+    r_BC
+    r_ABC
+end
+
 struct SimulationTime
     nt
     measure_ts
@@ -95,4 +115,30 @@ function Init_EdgeSquareLattice(subsys::Int)
     r_ABC = 0*subsys_size+1:3*subsys_size
 
     return EdgeSquareLattice(L, nbits, cell_num, subsys, subsys_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
+end
+
+function Init_VertexHoneyLattice(subsys::Int)
+    
+    # Dimensions
+    L = 4*subsys
+    k = 2 # Number of Qubits per unit cell
+    cell_num = L*L
+    nbits = k*cell_num
+    
+    # For TEN: define the projection onto the ususal multipartitions of the torus
+    subsys_size = k*L*subsys # Misleading NAME, be careful!
+    P_A = Diagonal(repeat(vcat(repeat([true], subsys_size), repeat([false], 3*subsys_size)), 2))
+    P_B = Diagonal(repeat(vcat(repeat([false], subsys_size), repeat([true], subsys_size), repeat([false], 2*subsys_size)), 2))
+    P_C = Diagonal(repeat(vcat(repeat([false], 2*subsys_size), repeat([true], subsys_size), repeat([false], subsys_size)), 2))
+    S = kron([false true; true false], Diagonal(repeat([true], k*L*L)));
+
+    # For TEE: refined qubit indices for diferent continous partitions
+    r_A = 0*subsys_size+1:1*subsys_size
+    r_B = 1*subsys_size+1:2*subsys_size
+    r_C = 2*subsys_size+1:3*subsys_size
+    r_AB = 0*subsys_size+1:2*subsys_size
+    r_BC = 1*subsys_size+1:3*subsys_size
+    r_ABC = 0*subsys_size+1:3*subsys_size
+
+    return VertexHoneyLattice(L, nbits, cell_num, subsys, subsys_size, P_A, P_B, P_C, S, r_A, r_B, r_C, r_AB, r_BC, r_ABC)
 end
