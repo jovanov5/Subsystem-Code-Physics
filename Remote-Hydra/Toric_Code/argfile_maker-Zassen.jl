1d48370504a2400c50292f733f233148745241e8
Jovan Jovanovic <jjovanovic996@gmail.com>
2024-10-01 11:02:50 +0100

    more stuff

--- Remote-Hydra/Toric_Code/argfile_maker-Zassen.jl
+++ Remote-Hydra/Toric_Code/argfile_maker-Zassen.jl
@@ -23,7 +23,7 @@ jul_path = Sys.BINDIR
 # Define the number of iterations for the for loop
 # L_arr = [30, 42, 54]
 # L_arr = [24, 30]
-L_arr = [10, 22, 40, 82]
+L_arr = [31]
 d = 1
 
 # p_all_arr = [(0.55, 0.0), (0.55, 0.1), (0.2, 0.21), (0.0, 0.15)] # Defined as list of doubles (p_f, p_b)
@@ -31,11 +31,11 @@ p_all_arr = [(0.75, 0.00), (0.75, 0.10)]
 
 # t_final = Int(2e4) # 1e5
 # n_t = 5 # 6
-t_mmt = Array{Int}(20:20:200).+50
+t_mmt = Array{Int}(20:20:40).+50
 
 n_subdiv = "all"
 
-number_of_repetitions = 100
+number_of_repetitions = 200
 number_of_experiments = number_of_repetitions*length(L_arr)*length(p_all_arr)
 offset = 0
 
