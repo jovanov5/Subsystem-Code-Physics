77c32ac3df7b3f1ac59b91e24f6a0dcd3edb2d9c
Jovan Jovanovic <jjovanovic996@gmail.com>
2024-10-08 14:10:15 +0100

    new fast ZH alg implementation testing

--- AdditionalStructure/Julia/Measurements.jl
+++ AdditionalStructure/Julia/Measurements.jl
@@ -170,6 +170,43 @@ function general_zassenhausen_correlator(state::MixedDestabilizer, system, rep_f
 
 end
 
+function general_zassenhausen_correlator_fast(state::MixedDestabilizer, system, rep_funtion::Function, def_function_fast::Function)
+    """this calculates the represented correlator between two sites (0, 0) and (all, all) of the state on a torus.
+    state            : state to be examined.
+    system           : system object that has knows everything!
+    rep_funtion      : function that returns the representative string operator.
+    deformator       : deformator to the representative
+    """
+
+    L = system.L
+    k = system.k
+    nbits = system.nbits
+    cell_num = system.cell_num
+
+    corr_function = zeros(Int, L, L)
+
+    for r_x  = 0:L-1
+        for r_y = 0:L-1
+            if r_x == 0 && r_y == 0
+                corr_function[r_x+1, r_y+1] = 1
+            else
+                # Removes non-commuting elements from the general (deformator, representative) group so we do not need to doible the system size.
+                deformator = def_function_fast((r_x, r_y), system)
+                # rep_function is an input
+                rep = rep_funtion((r_x, r_y), system)
+                # size_of_bff = 0
+                # zassenhausen_alg also in HelperTools.jl
+                dim_source, _ = zassenhausen_alg_fast(rep, deformator, stabilizerview(state), nbits)
+                dim_norm, _ = zassenhausen_alg_fast(deformator, stabilizerview(state), nbits)
+                corr_function[r_x+1, r_y+1] = Int(round(dim_source - dim_norm))
+            end
+        end
+    end
+
+    return corr_function
+
+end
+
 function particular_zassenhausen_correlator(r_2::Tuple{Int, Int}, state::MixedDestabilizer, system, rep_funtion::Function, deformator)
     """this calculates the represented correlator between two sites (0, 0) and (i, j) of the state on a torus.
     r_2              : coordinate of the second site.
@@ -194,6 +231,35 @@ function particular_zassenhausen_correlator(r_2::Tuple{Int, Int}, state::MixedDe
         # size_of_bff = 0
         dim_source, _ = zassenhausen_alg(rep, deformator, stabilizerview(state), nbits)
         dim_norm, _ = zassenhausen_alg(deformator, stabilizerview(state), nbits)
+        return Int(round(dim_source - dim_norm))
+    end
+end
+
+function particular_zassenhausen_correlator_fast(r_2::Tuple{Int, Int}, state::MixedDestabilizer, system, rep_funtion::Function, def_function_fast::Function)
+    """this calculates the represented correlator between two sites (0, 0) and (i, j) of the state on a torus.
+    r_2              : coordinate of the second site.
+    state            : state to be examined.
+    system           : system object that has knows everything!
+    rep_funtion      : function that returns the representative string operator.
+    deformator       : deformator to the representative
+    """
+
+    L = system.L
+    k = system.k
+    nbits = system.nbits
+    cell_num = system.cell_num
+
+    r_x = r_2[1]
+    r_y = r_2[2]
+
+    if r_x == 0 && r_y == 0
+        return 1
+    else
+        deformator = def_function_fast((r_x, r_y), system)
+        rep = rep_funtion((r_x, r_y), system)
+        # size_of_bff = 0
+        dim_source, _ = zassenhausen_alg_fast(rep, deformator, stabilizerview(state), nbits)
+        dim_norm, _ = zassenhausen_alg_fast(deformator, stabilizerview(state), nbits)
         return Int(round(dim_source - dim_norm))
     end
 end
@@ -262,6 +328,37 @@ function get_f_reprentative(r_2::Tuple{Int, Int}, system)
 end
 
 function get_e_deformator(system)
+    nbits = system.nbits
+    L = system.L
+    e_deformators = []
+
+    for cell_index = 0:L*L-1
+        push!(e_deformators, tc_stab(Plaquette, cell_index, system))
+    end
+
+    # The noncontractible loops are actually very important deformators.
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        Z_op_list[2*cell_index+1+1] = true
+    end
+    Z_loop_1 = PauliOperator(0x0, X_op_list, Z_op_list)
+    push!(e_deformators, Z_loop_1) # Up Loop
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        Z_op_list[2*L*cell_index+1] = true
+    end
+    Z_loop_2 = PauliOperator(0x0, X_op_list, Z_op_list)
+    push!(e_deformators, Z_loop_2) # Right Loop
+
+    return e_deformators
+
+end
+
+function get_e_deformator_fast(r_2::Tuple{Int, Int}, system) # no changes needed, This group is abelian!
     nbits = system.nbits
     L = system.L
     e_deformators = []
@@ -370,6 +467,68 @@ function get_f_deformator(system)
     end
     X_loop_2 = PauliOperator(0x0, X_op_list, Z_op_list)
     push!(f_deformators, X_loop_2) # Right Loop
+
+    return f_deformators
+
+end
+
+function get_f_deformator_fast(r_2::Tuple{Int, Int}, system)
+
+    nbits = system.nbits
+    L = system.L
+    f_deformators = []
+
+    problematic_index = r_2[1] + r_2[2]*L
+
+    for cell_index = 1:L*L-1
+        if cell_index != problematic_index
+            push!(f_deformators, tc_stab(Star, cell_index, system))
+        end
+    end
+
+    for cell_index = 0:L*L-1
+        if cell_index != problematic_index
+            push!(f_deformators, tc_stab(Plaquette, cell_index, system))
+        end
+    end
+
+    push!(f_deformators, tc_stab(Plaquette, problematic_index, system) * tc_stab(Star, problematic_index, system))
+    push!(f_deformators, tc_stab(Plaquette, 0, system) * tc_stab(Star, 0, system))
+
+    # The noncontractible loops are actually very important deformators.
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        Z_op_list[2*cell_index+1+1+L] = true # I want the Z to be to the left to X so that proper fermion symmetry deforms this string!
+    end
+    Z_loop_1 = PauliOperator(0x0, X_op_list, Z_op_list)
+    # push!(f_deformators, Z_loop_1) # Up Loop
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        Z_op_list[2*L*cell_index+1+2] = true # Simaraly, I want Z on top of Xs in the horizontal loop!
+        # The only issue is that the deformation power of this group is not adequate, but let us see how it goes!!! ;)
+    end
+    Z_loop_2 = PauliOperator(0x0, X_op_list, Z_op_list) 
+    # push!(f_deformators, Z_loop_2) # Right Loop
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        X_op_list[2*cell_index+1] = true
+    end
+    X_loop_1 = PauliOperator(0x0, X_op_list, Z_op_list)
+    push!(f_deformators, X_loop_1 * Z_loop_1) # Up Loop
+
+    Z_op_list = zeros(Bool, nbits)
+    X_op_list = zeros(Bool, nbits)
+    for cell_index = 0:L-1
+        X_op_list[2*L*cell_index+1+1] = true
+    end
+    X_loop_2 = PauliOperator(0x0, X_op_list, Z_op_list)
+    push!(f_deformators, X_loop_2 * Z_loop_2) # Right Loop
 
     return f_deformators
 
---
+++ Remote-Hydra/Toric_Code/argfile_maker-Zassen_fast.jl
@@ -0,0 +1,66 @@
+using Base.Filesystem  # for creaating directories etc
+using Random, Distributions  # random numbers
+using HDF5  # hdf5 files
+using QuantumClifford  # this is the stabilizer simulation package
+using Plots # for plotting
+# using Formatting # string formatting
+using LinearAlgebra # some useful matrices etc.
+using Missings # for missing values
+using JSON3 # for reading the JSON file
+dep_path = "../../AdditionalStructure/Julia/"
+include("$(dep_path)NewTypes.jl")
+include("$(dep_path)BitStringOps.jl")
+include("$(dep_path)Measurements.jl")
+include("$(dep_path)MICModels.jl")
+include("$(dep_path)ModelRuns.jl")
+include("$(dep_path)HelperTools.jl")
+include("$(dep_path)Iterators.jl");
+
+# Define the file path
+dir_path = @__DIR__
+jul_path = Sys.BINDIR
+
+# Define the number of iterations for the for loop
+# L_arr = [30, 42, 54]
+# L_arr = [24, 30]
+L_arr = [31]
+d = 1
+
+# p_all_arr = [(0.55, 0.0), (0.55, 0.1), (0.2, 0.21), (0.0, 0.15)] # Defined as list of doubles (p_f, p_b)
+p_all_arr = [(0.75, 0.00), (0.75, 0.10)]
+
+# t_final = Int(2e4) # 1e5
+# n_t = 5 # 6
+t_mmt = Array{Int}(20:20:40).+50
+
+n_subdiv = "all"
+
+number_of_repetitions = 200
+number_of_experiments = number_of_repetitions*length(L_arr)*length(p_all_arr)
+offset = 0
+
+# Generate the arg files
+for exp_index in (1 + offset):(number_of_experiments + offset)
+    usable_exp_index = exp_index - 1
+    usable_L_index = mod(div(usable_exp_index, length(p_all_arr)), length(L_arr))
+    usable_p_index = mod(usable_exp_index, length(p_all_arr))
+    L_index = usable_L_index + 1
+    p_index = usable_p_index + 1
+
+    L = L_arr[L_index]
+    p_f, p_b = p_all_arr[p_index]
+
+    subdiv_array = get_subdiv_array(L, n_subdiv)
+    arg_dict = Dict(
+        "L" => L,
+        "d" => d,
+        "p_f" => p_f,
+        "p_b" => p_b,
+        "t_mmt" => t_mmt,
+        "exp_index" => exp_index
+    )
+    open("$(dir_path)/data_zf/args/argfile_exp:$(exp_index).json", "w") do io
+        JSON3.pretty(io, arg_dict)
+    end
+end
+
